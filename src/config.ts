774b57b0d20ce1e1e0b9d52051807e3a4ca18e88
aditiays <72292189+ashtwoays@users.noreply.github.com>
2026-08-18 04:06:19 +0000

    fixing url

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const SITE = {
-    website: "https://simple-portfolio.vicbox.dev", // replace this with your deployed domain
+    website: "https://ashtwoays.github.io", // replace this with your deployed domain
     title: "Simple Portfolio",
     description: "A simple but beautiful portfolio created with Astro",
     tags: ["portfolio", "Resume cv", "Astro"],
